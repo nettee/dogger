fd26d7426b2fe3adc175e9fcb1eafcde2a7922c3
nettee <nettee.liu@gmail.com>
2021-02-12 22:05:49 +0800

    simple handler

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nettee/dogger
 
 go 1.13
+
+require github.com/gorilla/mux v1.8.0
--- server.go
+++ server.go
@@ -2,17 +2,50 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net"
 	"net/http"
 	"os"
 )
 
-func handleRoot(w http.ResponseWriter, r *http.Request) {
+func beforeHandle(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("> %v %v\n", r.Method, r.URL)
+	vars := mux.Vars(r)
+	for i, e := range vars {
+		fmt.Printf("%v='%v'\n", i, e)
+	}
 	fmt.Printf("%v\n", r.Body)
+}
+
+func handlePing(w http.ResponseWriter, r *http.Request) {
+	beforeHandle(w, r)
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	//w.WriteHeader(http.StatusInternalServerError)
+	//_, _ = w.Write([]byte("not implemented by dogger"))
+}
+
+func handleListContainers(w http.ResponseWriter, r *http.Request) {
+	beforeHandle(w, r)
+	w.WriteHeader(http.StatusInternalServerError)
+	_, _ = w.Write([]byte("not implemented by dogger"))
+}
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	beforeHandle(w, r)
 
 	w.WriteHeader(http.StatusInternalServerError)
 	_, _ = w.Write([]byte("not implemented by dogger"))
+}
+
+type Route struct {
+	Path    string
+	Handler func(w http.ResponseWriter, r *http.Request)
+}
+
+var routes = []Route{
+	{"/_ping", handlePing},
+	{"/v1.24/containers/{id}/json", handleListContainers},
 }
 
 func serve(sockFile string) {
@@ -24,7 +57,13 @@ func serve(sockFile string) {
 
 	fmt.Printf("listen to %v...\n", sockFile)
 
-	http.HandleFunc("/", handleRoot)
+	router := mux.NewRouter()
+	for _, route := range routes {
+		router.HandleFunc(route.Path, route.Handler)
+	}
+	router.NotFoundHandler = http.HandlerFunc(handleRoot)
+	http.Handle("/", router)
+
 	_ = http.Serve(listener, nil)
 }
 
